47ba8385e60c2234798abf8a5e9fb244d5e5107d
Oddgeir Gitlestad <oddgeir.gitestad@gmail.com>
2023-04-20 13:28:48 +0200

    Exposed remote component

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'remote1',
+        remotes: {
+          host: `host@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './remote': './src/pages/remote.tsx',
+        },
+        shared: {
+          // whatever else
+        },
+      })
+    );
+
+    return config;
+  },
+};
